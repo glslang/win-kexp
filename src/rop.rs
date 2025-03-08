d17729b25e88cb9a350fa5eb5e43fed25b3f5606
Gonçalo Carvalho <glslang@gmail.com>
2025-03-08 13:41:18 +0000

    feat(pool, rop): enhance AnonymousPipe and update PE header parsing
    - Added `buf_size` field to `AnonymousPipe` struct to track buffer size
    - Implemented `drain()` method to clear pipe contents
    - Updated ROP module to use windows-rs `IMAGE_NT_HEADERS64` instead of custom struct
    - Simplified PE header parsing by leveraging windows-rs types

--- src/rop.rs
+++ src/rop.rs
@@ -1,6 +1,7 @@
 use goblin::pe::{options::ParseOptions, section_table::IMAGE_SCN_CNT_CODE};
 use thiserror::Error;
 use windows::Win32::Foundation::HMODULE;
+use windows::Win32::System::Diagnostics::Debug::IMAGE_NT_HEADERS64;
 use windows::Win32::System::SystemServices::IMAGE_DOS_HEADER;
 
 #[macro_export]
@@ -41,68 +42,6 @@ macro_rules! concat_rop_chain_to_buffer {
     }};
 }
 
-#[repr(C)]
-#[derive(Copy, Clone)]
-pub struct ImageFileHeader {
-    pub machine: u16,
-    pub number_of_sections: u16,
-    pub time_date_stamp: u32,
-    pub pointer_to_symbol_table: u32,
-    pub number_of_symbols: u32,
-    pub size_of_optional_header: u16,
-    pub characteristics: u16,
-}
-
-#[repr(C)]
-#[derive(Copy, Clone)]
-pub struct ImageDataDirectory {
-    pub virtual_address: u32,
-    pub size: u32,
-}
-
-#[repr(C)]
-#[derive(Copy, Clone)]
-pub struct ImageOptionalHeader64 {
-    pub magic: u16,
-    pub major_linker_version: u8,
-    pub minor_linker_version: u8,
-    pub size_of_code: u32,
-    pub size_of_initialized_data: u32,
-    pub size_of_uninitialized_data: u32,
-    pub address_of_entry_point: u32,
-    pub base_of_code: u32,
-    pub image_base: u64,
-    pub section_alignment: u32,
-    pub file_alignment: u32,
-    pub major_operating_system_version: u16,
-    pub minor_operating_system_version: u16,
-    pub major_image_version: u16,
-    pub minor_image_version: u16,
-    pub major_subsystem_version: u16,
-    pub minor_subsystem_version: u16,
-    pub win32_version_value: u32,
-    pub size_of_image: u32,
-    pub size_of_headers: u32,
-    pub check_sum: u32,
-    pub subsystem: u16,
-    pub dll_characteristics: u16,
-    pub size_of_stack_reserve: u64,
-    pub size_of_stack_commit: u64,
-    pub size_of_heap_reserve: u64,
-    pub size_of_heap_commit: u64,
-    pub loader_flags: u32,
-    pub number_of_rva_and_sizes: u32,
-    pub data_directory: [ImageDataDirectory; 16],
-}
-
-#[repr(C)]
-#[derive(Copy, Clone)]
-pub struct ImageNtHeaders64 {
-    pub signature: u32,
-    pub file_header: ImageFileHeader,
-    pub optional_header: ImageOptionalHeader64,
-}
-
 #[derive(Error, Debug)]
 pub enum PeError {
     #[error("Failed to find executable sections")]
@@ -115,8 +54,8 @@ pub fn get_executable_sections(module: HMODULE) -> Result<Vec<(u64, Vec<u8>)>, P
     // Get module information using VirtualQuery
     let dos_header = unsafe { *(module.0 as *const IMAGE_DOS_HEADER) };
     let nt_headers =
-        unsafe { *(module.0.offset(dos_header.e_lfanew as isize) as *const ImageNtHeaders64) };
-    let image_size = nt_headers.optional_header.size_of_image as usize;
+        unsafe { *(module.0.offset(dos_header.e_lfanew as isize) as *const IMAGE_NT_HEADERS64) };
+    let image_size = nt_headers.OptionalHeader.SizeOfImage as usize;
 
     // Convert module to bytes
     let pe_bytes = unsafe { std::slice::from_raw_parts(module.0 as *const u8, image_size) };
